ad3773946298984eb49cd9af7aa452eb0f3362ff
MaxbIch <94921982+MaxbIch@users.noreply.github.com>
2025-03-30 20:39:39 +0300

    hyu

---
+++ mostauto/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+
+
+function PrivacyPolicy() {
+  return (
+    <div className="privacy-container">
+      <h1>Политика конфиденциальности</h1>
+      <p>Здесь размещается информация о политике конфиденциальности...</p>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
